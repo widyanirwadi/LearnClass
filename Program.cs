9ca1128e80c1ce84af28deb95db38c95047fab64
WIDYAN IRWADI <015021@npid.com>
2023-06-13 08:32:50 +0700

    Initial Commit of LearnClass Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LearnClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(20, 10);
+            double area  = rectangle.CalculateArea();
+            double parimeter = rectangle.CalculateParimeter();
+            Console.WriteLine("The Area of the Rectangle is {0}", area);
+            Console.WriteLine("The Parimeter of the Rectangle is {0}", parimeter);
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnClass
+{
+    /* Class */
+    public class Rectangle
+    {
+        /* Property */
+        private double Length { get; set; }
+
+        private double Width { get; set; }
+
+        /* Constructor */
+        public Rectangle (double length, double width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        /* Method */
+        public double CalculateArea()
+        {
+            return Length * Width;
+        }
+
+        public double CalculateParimeter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+}
